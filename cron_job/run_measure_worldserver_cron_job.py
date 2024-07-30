e60229b94cfd65b6409a00acabeaacba17b98d03
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-07-29 20:11:27 -0400

    improve doc

--- cron_job/run_measure_worldserver_cron_job.py
+++ cron_job/run_measure_worldserver_cron_job.py
@@ -1,5 +1,15 @@
-#!/usr/bin/env /home/ubuntu/git_repos/acore_server_monitoring_measurement/.venv/bin/python
+#!/usr/bin/env /home/ubuntu/git_repos/acore_server_monitoring_measurement-project/.venv/bin/python
 # -*- coding: utf-8 -*-
+
+"""
+Run this script in GNU screen session when EC2 instance is started, it will
+measure worldserver status every 5 minutes and write the result to DynamoDB.
+
+You can run the following command to test it in worldserver EC2 instance,
+you should see one item is created every 5 minutes in DynamoDB table::
+
+    /home/ubuntu/git_repos/acore_server_monitoring_measurement/.venv/bin/python /home/ubuntu/git_repos/acore_server_monitoring_measurement-project/cron_job/run_measure_worldserver_cron_job.py
+"""
 
 from acore_server_monitoring_measurement.cron_job import run_log_to_ec2_tag_cron_job
 
